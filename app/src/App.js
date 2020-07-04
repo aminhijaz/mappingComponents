206a0cbc3cad9d1ab430e53519b933fb343e1eab
Amin HIjaz <aminhijaz2001@gmail.com>
2020-07-04 22:52:14 +0300

    Activity is ready

---
+++ app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Students from './Students'
+class App extends React.Component {
+  render() {
+  // use mapping here
+    return (
+      <div>
+      </div>
+    );
+  
+  }
+}
+
+export default App;
---
+++ app/src/Students.js
@@ -0,0 +1,69 @@
+const Students = [
+    {
+        id: "1",
+        name: "Diana Aldeek"
+    },
+    {
+        id: "2",
+        name: "Lana",
+    },
+    {
+        id: "3",
+        name: "Nour Sharia",
+    },
+    {
+        id: "4",
+        name: "Tamer Nabulsi",
+    },
+    {
+        id: "5",
+        name: "Mousa Bsharat",
+    },
+    {
+        id: "6",
+        name: "Shadya Amjad",
+    },
+    {
+        id: "7",
+        name: "Mohammad Alsallamin",
+    },
+    {
+        id: "8",
+        name: "Rania Jadallah",
+    },
+    {
+        id: "9",
+        name: "Maisa Farwanah",
+    },
+    {
+        id: "10",
+        name: "Nour Malakah",
+    },
+    {
+        id: "11",
+        name: "Kareem Maraytah",
+    },
+    {
+        id: "12",
+        name: "Ehab Sarsour",
+    },
+    {
+        id: "13",
+        name: "Amro Abou-Hachem",
+    },
+    {
+        id: "14",
+        name: "Monya Assi",
+    },
+    {
+        id: "15",
+        name: "Naeem Qawasmeh",
+    },
+    {
+        id: "16",
+        name: "Hadeel Sadiq",
+    },
+
+]
+
+export default Students
